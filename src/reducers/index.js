b6918d2c589d31cbd49c20e4d9b88c0c5c6d6ebd
Erasmo Hernández <erasmoh@gmail.com>
2022-01-26 00:08:59 -0300

    Fully working app

---
+++ src/reducers/index.js
@@ -0,0 +1,10 @@
+import { combineReducers } from "redux";
+import characters from "./charactersReducer";
+import heroes from "./heroesReducers";
+
+const rootReducer = combineReducers({
+  characters,
+  heroes,
+});
+
+export default rootReducer;
